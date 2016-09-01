61a6a50eba2044cd8cf277654d33830403753587
Steve Henderson <hevets@gmail.com>
2016-09-01 10:33:43 -0700

    APM Menu Bar App
    Setting up initial menubar app to track a users apm

--- apm/AppDelegate.swift
+++ apm/AppDelegate.swift
@@ -11,17 +11,28 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    let statusItem = NSStatusBar.system().statusItem(withLength: -2)
+    
     @IBOutlet weak var window: NSWindow!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = #selector(self.printQuote)
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
+    func printQuote(sender:AnyObject) {
+        let quoteText = "Hello"
+        let quoteAuthor = "Hevets"
+        
+        print("\(quoteText) \n--'\(quoteAuthor)'")
+    }
 
 }
 
